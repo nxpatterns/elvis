e78e8e92dd9b5075c8e407a255e8510563d015dd
webia1 <webia1.github@gmail.com>
2025-01-15 16:35:47 +0100

    Initialize mw-base application with NestJS setup

---
+++ apps/mw-base/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'mw-base',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/mw-base',
+};
